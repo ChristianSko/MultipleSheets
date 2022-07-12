a2993be496941977739a5c156d0116487767c40e
Christian Skorobogatow <christiansko@icloud.com>
2022-07-13 00:57:45 +0200

    Initial Commit

---
+++ MultipleSheets/MultipleSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleSheetsApp.swift
+//  MultipleSheets
+//
+//  Created by Christian Skorobogatow on 13/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MultipleSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
