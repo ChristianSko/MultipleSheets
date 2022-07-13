2084b344b742160b13a3c5acf475cbd877b4d052
Christian Skorobogatow <christiansko@icloud.com>
2022-07-13 02:07:15 +0200

    feat(): scalable multiple sheets

--- MultipleSheets/ContentView.swift
+++ MultipleSheets/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+
+struct RandomModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+}
+
 struct ContentView: View {
+    
+    @State var selectedModel: RandomModel? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack(spacing: 20) {
+                
+                ForEach(0..<50) { index in
+                    Button("Button \(index)") {
+                        selectedModel = RandomModel(title: "\(index)")
+                    }
+                }
+            }
+            .sheet(item: $selectedModel) { model in
+                NextScreen(selectedModel: model)
+            }
+        }
+    }
+}
+
+struct NextScreen: View {
+    
+    let selectedModel: RandomModel
+    
+    var body: some View {
+        Text(selectedModel.title)
+            .font(.largeTitle)
     }
 }
 
